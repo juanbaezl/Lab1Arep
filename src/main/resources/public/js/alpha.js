da126a22522799e3eb1c8916351bc06e315ffd5c
juanbaezl <juan.baez-l@mail.escuelaing.edu.co>
2022-08-17 22:09:23 -0500

    Finalizacion request api

--- src/main/resources/public/js/alpha.js
+++ src/main/resources/public/js/alpha.js
@@ -1,14 +1,23 @@
 class Alpha extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { function: "intraday", symbol: "" };
-    this.handleChange = this.handleChangeInput.bind(this);
-    this.handleChange = this.handleChangeSelect.bind(this);
+    this.state = {
+      function: "intraday",
+      symbol: "",
+      jsonRes: {},
+      interval: "",
+    };
+    this.handleChangeSelect = this.handleChangeSelect.bind(this);
+    this.handleChangeInput = this.handleChangeInput.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangeInputInterval = this.handleChangeInputInterval.bind(this);
   }
 
   handleChangeInput(event) {
     this.setState({ symbol: event.target.value });
+  }
+  handleChangeInputInterval(event) {
+    this.setState({ interval: event.target.value });
   }
 
   handleChangeSelect(event) {
@@ -16,8 +25,25 @@ class Alpha extends React.Component {
   }
 
   handleSubmit(event) {
-    alert("Your favorite flavor is: " + this.state.value);
+    this.loadPostMsg();
     event.preventDefault();
+  }
+
+  loadPostMsg() {
+    let url =
+      "/stock?function=" + this.state.function + "&symbol=" + this.state.symbol;
+    if (this.state.function == "intraday") {
+      url += "&interval=" + this.state.interval;
+    }
+    console.log(url);
+    fetch(url, { method: "GET" })
+      .then((x) => x.json())
+      .then((y) => {
+        //Create a new visualizer object
+        var _visualizer = new visualizer($("#resMSG"));
+        //Visualize the demo json object
+        _visualizer.visualize(y);
+      });
   }
 
   render() {
@@ -25,9 +51,8 @@ class Alpha extends React.Component {
       <div>
         <h1>AlphaVantage Info</h1>
         <form onSubmit={this.handleSubmit}>
-          <label for="function">Function:</label>
+          <label>Function:</label>
           <select
-            name="function"
             value={this.state.function}
             onChange={this.handleChangeSelect}
           >
@@ -36,10 +61,24 @@ class Alpha extends React.Component {
             <option value="weekly">Weekly</option>
             <option value="monthly">Monthly</option>
           </select>
-          <label for="symbol">Symbol:</label>
+          {this.state.function == "intraday" ? (
+            <div>
+              <label>Interval:</label>
+              <input
+                required
+                type="text"
+                placeholder="ej. 1min, 2min ..."
+                value={this.state.interval}
+                onChange={this.handleChangeInputInterval}
+              ></input>
+            </div>
+          ) : (
+            <div></div>
+          )}
+          <label>Symbol:</label>
           <input
+            required
             type="text"
-            name="symbol"
             placeholder="ej. IBM, MSFT..."
             value={this.state.symbol}
             onChange={this.handleChangeInput}
